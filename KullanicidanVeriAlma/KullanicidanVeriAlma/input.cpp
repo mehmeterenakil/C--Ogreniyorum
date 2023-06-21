cd302203cfdafac1bf84e56cf6b286b5094f44ce
mehmeterenakil <mehmeterenakil8@gmail.com>
2023-06-21 20:33:45 +0300

    Kullanıcıdan veri alma
    Kullanıcıdan veri alma kodu

---
+++ KullanicidanVeriAlma/KullanicidanVeriAlma/input.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+	cout << "Merhaba" << endl;
+
+	int en, boy, alan;
+	
+	cout << "Lutfen dikdortgenin enini giriniz:";
+	cin >> en;
+	cout << endl;
+
+	cout << "Lutfen dikdortgenin boyunu giriniz:";
+	cin >> boy;
+
+	cout << endl;  
+
+	
+	alan = en * boy;
+	cout << "Dikdortgenin alani = " << alan << " mertrekaredir." << endl;
+
+	cout << endl;
+
+	return 0;
+
+
+}
